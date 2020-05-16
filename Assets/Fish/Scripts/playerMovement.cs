8bcf088fc07b970aaf79546965baa26bef1ded45
Fish On Water <luismiguelfigueiras@gmail.com>
2020-05-16 18:44:05 +0100

    Walking done. Starting to put some random walls to define game space for exploration.

---
+++ Assets/Fish/Scripts/playerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public CharacterController playerController;
+
+    public float playerSpeed = 12f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        playerController.Move(move * playerSpeed * Time.deltaTime);
+    }
+}
